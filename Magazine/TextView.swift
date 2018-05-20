247b498b58c5e86282c117d4089660c25c6c9080
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-05-20 22:58:54 +0700

    Implement first custom text view

--- Magazine/TextView.swift
+++ Magazine/TextView.swift
@@ -2,4 +2,20 @@ import UIKit
 import CoreText
 
 class TextView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.textMatrix = .identity
+        context.translateBy(x: 0, y: bounds.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+
+        let path = CGMutablePath()
+        path.addRect(bounds)
+
+        let attrString = NSAttributedString(string: "Hello World")
+        let framesetter = CTFramesetterCreateWithAttributedString(attrString as CFAttributedString)
+        let frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, attrString.length), path, nil)
+        CTFrameDraw(frame, context)
+    }
 }
